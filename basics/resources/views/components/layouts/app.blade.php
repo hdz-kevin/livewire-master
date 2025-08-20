631cb5c0f75017adb61b5cd36867dfe884716189
hdz-kevin <kevinhdez592@gmail.com>
2025-08-19 18:28:37 -0600

    page components

---
+++ basics/resources/views/components/layouts/app.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+		<title>{{ $title ?? 'Default Title' }}</title>
+
+    @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
+      @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @endif
+	</head>
+
+	<body>
+
+		<div class="flex">
+			<nav class="bg-gray-800 text-white h-screen w-60 inline-block pl-4 pt-6">
+				<a @class(['block mb-3', 'font-bold' => request()->is('/')]) href="/">Todos</a>
+				<a @class(['block mb-3', 'font-bold' => request()->is('counter')]) href="/counter">Counter</a>
+			</nav>
+
+			{{ $slot }}
+		</div>
+	</body>
+</html>
